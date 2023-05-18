2a0d54426b2b940c6688c4ca71213de80c16f51f
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2023-05-18 20:29:34 -0300

    feat: summary 03

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const SummaryContainer = styled.section`
-  grid-column: 2;
+  grid-column: 2 / 3;
 
   width: 100%;
   max-width: 1120px;
@@ -9,9 +9,26 @@ export const SummaryContainer = styled.section`
   padding: 0 1.5rem;
 
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 2rem;
+  grid-template-columns: 23rem 23rem 23rem;
+  gap: 1rem;
   margin-top: -5rem;
 `;
 
-export const SummaryCard = styled.div``;
+export const SummaryCard = styled.div`
+  background: ${props => props.theme['gray-500']};
+  border-radius: 6px;
+  padding: 2rem;
+  box-shadow: 0px 0px 10px 5px #00000010;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  strong {
+    display: block;
+    margin-top: 1rem;
+    font-size: 2rem;
+  }
+`;
