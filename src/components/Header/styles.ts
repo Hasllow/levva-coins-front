e8d550e2dcfa65a2c19a06dad176c3f74792572c
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2023-05-18 16:13:12 -0300

    feat: header 02

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,20 @@
 import styled from 'styled-components';
 
-export const HeaderContainer = styled.header``;
+export const HeaderContainer = styled.header`
+  background: ${props => props.theme['gray-600']};
+  padding: 2.5rem 0 7.5rem;
+
+  grid-column-start: 2;
+  grid-column-end: 3;
+
+  display: grid;
+  grid-template-columns: 1fr 70rem 7rem 1fr;
+`;
+
 export const HeaderContent = styled.div``;
+
 export const NewCategoryButton = styled.button``;
+
 export const NewTransactionButton = styled.button``;
+
 export const UserAvatar = styled.img``;
