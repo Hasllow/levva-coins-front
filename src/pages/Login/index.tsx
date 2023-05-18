0d10ac56fb07a39eb7278d2df0207f03f7b8b449
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2023-05-18 14:10:40 -0300

    feat: email e senha

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,7 +3,11 @@ import { AuthLayout } from '../../Layouts/AuthLayout';
 export const Login = () => {
   return (
     <AuthLayout title="Login" subtitle="Gerenciar suas entradas e saídas nunca foi tão simples.">
-      <p>Login</p>
+      <form>
+        <input type="email" name="" id="" placeholder="E-mail" />
+        <input type="password" name="" id="" placeholder="Senha" />
+        <button type="submit">Entrar</button>
+      </form>
     </AuthLayout>
   );
 };
--- src/pages/NewAccount/index.tsx
+++ src/pages/NewAccount/index.tsx
@@ -3,7 +3,12 @@ import { AuthLayout } from '../../Layouts/AuthLayout';
 export const NewAccount = () => {
   return (
     <AuthLayout title="Cadastro" subtitle="Crie sua conta e começe a gerenciar suas finanças.">
-      <p>New Account</p>
+      <form>
+        <input type="name" name="" id="" placeholder="Nome e sobrenome" />
+        <input type="email" name="" id="" placeholder="E-mail" />
+        <input type="password" name="" id="" placeholder="Senha" />
+        <button type="submit">Criar conta</button>
+      </form>
     </AuthLayout>
   );
 };
