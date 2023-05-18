e67e37f815fbec84a980a998d3d25a3cd97af03a
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2023-05-18 10:34:20 -0300

    feat: add routes

---
+++ src/Router.tsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from 'react-router-dom';
+
+export function Router() {
+  <Routes>
+    <Route index path="/login" />
+    <Route path="/new-account" />
+    <Route path="/home" />
+  </Routes>;
+}
