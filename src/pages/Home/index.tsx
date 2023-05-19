f3151161df807c6ee7d926a981db94e8538c013c
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2023-05-18 23:04:11 -0300

    feat: table 01

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import { Header } from '../../components/Header';
 import { SearchForm } from '../../components/SearchForm';
 import { Summary } from '../../components/Summary';
-import { HomeWrapper } from './styles';
+import { HomeWrapper, PriceHighlight, TransactionTable, TransactionsContainer } from './styles';
 
 export const Home = () => {
   return (
@@ -10,6 +10,30 @@ export const Home = () => {
       <Summary />
 
       <SearchForm />
+
+      <TransactionsContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hambúrguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionsContainer>
     </HomeWrapper>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,3 +4,39 @@ export const HomeWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 77em 1fr;
 `;
+
+export const TransactionsContainer = styled.main`
+  grid-column: 3/2;
+  width: 100%;
+  max-width: 1120px;
+  margin-top: 1rem;
+`;
+
+export const TransactionTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${props => props.theme['gray-500']};
+
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`;
+
+interface PriceHighlightProps {
+  variant: 'income' | 'outcome';
+}
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+  color: ${props =>
+    props.variant === 'income' ? props.theme['green-500'] : props.theme['red-500']};
+`;
