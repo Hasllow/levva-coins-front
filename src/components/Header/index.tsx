0e1720a339e28f15f99a83d8aed4f27e42912037
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2023-05-18 22:10:44 -0300

    feat: modal 04

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,6 +10,7 @@ import {
 
 import levvaCoinsLogo from '../../assets/logo.svg';
 import { Modal } from '../Modal';
+import { Form, FormButton, FormInput } from '../../styles/global';
 
 export const Header = () => {
   const newCategoryButton: ReactNode = <NewCategoryButton>Nova Categoria</NewCategoryButton>;
@@ -25,14 +26,19 @@ export const Header = () => {
 
         <div>
           <Modal title="Nova categoria" trigger={newCategoryButton}>
-            p
+            <p>categoria</p>
           </Modal>
           <Modal title="Nova transação" trigger={newTransactionButton}>
-            p
+            <p>transacao</p>
           </Modal>
         </div>
         <Modal title="Meu perfil" trigger={userAvatar}>
-          p
+          <Form>
+            <UserAvatar src="https:\\github.com/jemluz.png" alt="Jemima Luz" variant="large" />
+            <FormInput type="text" value="Jemima Luz" />
+            <FormInput type="email" placeholder="jemima.luz@levva.io" disabled />
+            <FormButton type="submit">Atualizar</FormButton>
+          </Form>
         </Modal>
       </HeaderContent>
     </HeaderContainer>
--- src/pages/NewAccount/index.tsx
+++ src/pages/NewAccount/index.tsx
@@ -5,7 +5,7 @@ export const NewAccount = () => {
   return (
     <AuthLayout title="Cadastro" subtitle="Crie sua conta e começe a gerenciar suas finanças.">
       <Form>
-        <FormInput type="name" name="" id="" placeholder="Nome e sobrenome" />
+        <FormInput type="text" name="" id="" placeholder="Nome e sobrenome" />
         <FormInput type="email" name="" id="" placeholder="E-mail" />
         <FormInput type="password" name="" id="" placeholder="Senha" />
         <FormButton type="submit">Criar conta</FormButton>
